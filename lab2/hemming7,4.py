0a8e0e62fb8899d50ab0c64fbbf7398d799da482
ExcaliBBur <excalibburn@shinra-tensei.ru>
2021-10-11 15:09:49 +0300

    papki

---
+++ lab2/hemming7,4.py
@@ -0,0 +1,46 @@
+a = input()
+flag = 0
+if len(a) != 7:
+    print('Длина набора не равна 7.')
+    flag = 1
+else:
+    for i in a:
+        if int(i) != 0 and int(i) != 1:
+            print('Не двоичная запись числа')
+            flag = 1
+if flag == 0:    
+    k = 0   
+    r1 = a[0]
+    r2 = a[1]
+    r3 = a[3]
+    modr1 = (a[2] + a[4] + a[6]).count('1') % 2
+    modr2 = (a[2] + a[5] + a[6]).count('1') % 2
+    modr3 = (a[4] + a[5] + a[6]).count('1') % 2
+    if r1 != str(modr1):
+        k += 1
+    if r2 != str(modr2):
+        k += 2
+    if r3 != str(modr3):
+        k += 4
+    if (k > 0):
+        if k == 1:
+            if a[k-1] == '0':
+                a = '1' + a[k:]
+            else:
+                a = '0' + a[k:]
+        elif k == 7:
+            if a[k-1] == '0':
+                a = a[:k-1] + '1'
+            else:
+                a = a[:k-1] + '0'
+        elif a[k-1] == '0':
+            a = a[:k-1] + '1' + a[k:]
+        else:
+            a = a[:k-1] + '0' + a[k:]
+        print(f'Ошибка в бите №{k}')
+        print('Правильное сообщение(только информационные биты) -',a[2]+a[4]+a[5]+a[6])
+    else:
+        print('Ошибок нет, код доставлен верно')
+        print(a[2]+a[4]+a[5]+a[6])
+
+
---
+++ lab3/lab3_1.py
@@ -0,0 +1,54 @@
+import re
+import pygame
+
+# =<| - смайлик
+print('Номер ИСУ = 335989')
+print('335989 % 5 =',335989%5,';','335989 % 4 =',335989%4,';','3354989 % 7 =',335989%7)
+print('Мой смайлик - "=<|"')
+Test1 = '=<|=<|=<|=<|=<|=<|=<|=<|=<|=<|=<|=<|=<|=<|=<|=<|=<|=<|=<|=<|=<|'
+Test2 = '=<|=<|:-)=<X|=<|'
+Test3 = 'X-{O8<{P=<|=<'
+Test4 = ''
+Test5 = '12341235123049878123047123074123087410237401923874-12375r01347059124091237084517230846'
+Mass = []
+k = 1
+Mass.append(Test1)
+Mass.append(Test2)
+Mass.append(Test3)
+Mass.append(Test4)
+Mass.append(Test5)
+for i in Mass:
+	result_regex = len(re.findall('=<\|', i))
+	result_python = i.count('=<|')
+	counter = result_regex
+	if result_regex == result_python:
+		if counter % 100 >= 11 and counter % 100 < 20:
+			print(f'Test{k} прошёл успешно, в тексте всего {result_regex} смайликов "=<|".')
+		elif counter % 10 == 1:
+			print(f'Test{k} прошёл успешно, в тексте всего {result_regex} смайлик "=<|".')
+		elif counter % 10 == 2 or counter % 10 == 3 or counter % 10 == 4:
+			print(f'Test{k} прошёл успешно, в тексте всего {result_regex} смайликa "=<|".')
+		else:
+			print(f'Test{k} прошёл успешно, в тексте всего {result_regex} смайликов "=<|".')
+	else:
+		print(f'Test{k} не прошёл, автор сделал что-то не так, исправляй.')
+	k += 1
+print('Для рисовалки смайлика тыкните Enter')
+_ = input()
+s = 1
+while s:
+	pygame.init()
+	keys = pygame.key.get_pressed()
+	pygame.display.set_caption('Рисовалка смайлика')
+	win = pygame.display.set_mode((700,500))
+	pygame.draw.line(win,(255,255,255),(100,210),(250,210),7)
+	pygame.draw.line(win,(255,255,255),(100,230),(250,230),7)
+	pygame.draw.line(win,(255,255,255),(270,220),(420,150),7)
+	pygame.draw.line(win,(255,255,255),(270,220),(420,290),7)
+	pygame.draw.line(win,(255,255,255),(470,110),(470,330),7)
+	for k in pygame.event.get():
+		if k.type == pygame.QUIT:
+			s = False
+	if keys[pygame.K_ESCAPE]:
+		s = False
+	pygame.display.update()
